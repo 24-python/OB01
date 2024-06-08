51afd1a7670007bcbde41118ce6491c93e2fb471
24-python <24_python@mail.ru>
2024-06-08 20:02:27 +0300

    задача с контактами

--- contact.py
+++ contact.py
@@ -1,12 +1,32 @@
 class Contact():
-    def __init__(self, first_name, last_name, phone, email):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.phone = phone
-        self.email = email
-    def info(self):
-        print("First name: " + self.first_name)
-        print("Last name: " + self.last_name)
-        print("Phone: " + self.phone)
-        print("Email: " + self.email)
+    def __init__(self):
+        self.contact_list = []
+
+    def add_contact(self, first_name, last_name, phone, email):
+        new_contact = {
+            'Имя': first_name,
+            'Фамилия': last_name,
+            'Телефон': phone,
+            'Email': email
+        }
+        self.contact_list.append(new_contact)
+
+    def remove_contact(self, first_name, last_name):
+        for contact in self.contact_list:
+            if contact['Имя'] == first_name and contact['Фамилия'] == last_name:
+                self.contact_list.remove(contact)
+
+    def view_contacts(self, first_name, last_name):
+        for contact in self.contact_list:
+            if contact['Имя'] == first_name and contact['Фамилия'] == last_name:
+                print(contact)
+
+c = Contact()
+c.add_contact('John', 'Doe', '555-555-5555', 'tqK1v@example.com')
+c.add_contact('Jane', 'Jo', '555-555-5555', 'tqK2v@example.com')
+c.add_contact('Joe', 'Due', '555-555-5555', 'tqK23@example.com')
+
+
+
+c.view_contacts('John', 'Doe')
 
